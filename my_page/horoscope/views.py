ae40d35ebcb562ff79649887953da34699f1c8f7
San4esWebDeveloper <alex.bolshakov17032005@gmail.com>
2022-05-14 10:51:55 +0300

    homework

---
+++ my_page/horoscope/views.py
@@ -0,0 +1,19 @@
+import http
+import imp
+from urllib import response
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def leo(request):
+    return HttpResponse("Знак зодиака - лев")
+
+def scorpio(request):
+    return HttpResponse("Знак зодиака - скорпион")
+
+def aries(request):
+    return HttpResponse("Знак зодиака - овен")
+
+def Taurus(request):
+    return HttpResponse("Знак зодиака - телец")
